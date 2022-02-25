c252d2dd086f4fbd0a686893cd6e243f722b50da
MaHDiaLaGaB <elmahdi.alagab@gmail.com>
2022-02-25 18:39:44 +0300

    remove secret key from setting

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -11,16 +11,18 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b)5kpe5&4967=c_!y7!2y(+x6lj6q9pexlex$7a8l4$x-n$49j'
+my_secret_key = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
